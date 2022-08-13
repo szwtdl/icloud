27d3e874e7b011fa435360c9aac08ac9b225a08a
szwtdl <szpengjian@gmail.com>
2022-08-13 19:19:19 +0800

    add phpunit Test

--- tests/iCloudTest.php
+++ tests/iCloudTest.php
@@ -16,7 +16,24 @@ class iCloudTest extends TestCase
             'msg' => 'ok',
             'data' => []
         ]);
-        $result = $icloud->login('szpengjian@gmail.com','12345678');
+        $result = $icloud->login('szpengjian@gmail.com', '12345678');
+        $this->assertIsArray($result);
+        $this->assertEquals([
+            'code' => 200,
+            'msg' => 'ok',
+            'data' => []
+        ], $result);
+    }
+
+    public function testDownload()
+    {
+        $icloud = \Mockery::mock(Application::class, ['client_id' => '111', 'client_key', 'domain' => 'https://localhost:8080']);
+        $icloud->shouldReceive('download')->once()->andReturn([
+            'code' => 200,
+            'msg' => 'ok',
+            'data' => []
+        ]);
+        $result = $icloud->download('szpengjian@gmail.com', '12345678');
         $this->assertIsArray($result);
         $this->assertEquals([
             'code' => 200,
