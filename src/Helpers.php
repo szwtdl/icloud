9ab849d9554e87d7e8637079778d62246b18223e
szwtdl <szpengjian@gmail.com>
2022-08-13 20:20:45 +0800

    fix: modified files

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,13 +1,16 @@
 <?php
 
-/**
- * print
- * @param $arr
- * @return void
- */
-function dd($arr)
-{
-    echo "<pre>";
-    print_r($arr);
-    exit();
+if (!function_exists('dd')) {
+    /**
+     * print
+     * @param $arr
+     * @return void
+     */
+    function dd($arr)
+    {
+        echo "<pre>";
+        print_r($arr);
+        exit();
+    }
+
 }
